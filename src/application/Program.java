87f96273a904ff026eda1f90573d0cb1528299d7
Filipe <filipeandrade000@gmail.com>
2022-11-05 17:56:30 -0300

    Codigo com mais informacoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,8 +14,11 @@ public class Program {
 	
 	public static void main(String[] args) {
 
+		
+		
 		System.out.println(ANSI_YELLOW+"=====Vagas Devs Junior no Catho====="+ANSI_RESET);
-
+		
+		
 		for (int j = 0; j < 10; j++) {
 			try {
 				Document url = Jsoup.connect(
@@ -30,19 +33,36 @@ public class Program {
 					Element info = informacoes.get(i);
 
 					System.out.print(ANSI_YELLOW+"\nPUBLICACAO " + (i + j) + "#:\n"+ANSI_RESET);
-					String trabalho = info.getElementsByTag("a").text();
+					String cidade = info.getElementsByClass("sc-iyvyFf kIwChr").text();
+					String trabalho = info.getElementsByTag("a").text().replace(cidade, ""); //Substituindo Cidade por vazio, no Trabalho
 					System.out.println(ANSI_YELLOW+"Trabalho: "+ANSI_RESET + trabalho);
 					String empresa = info.getElementsByTag("p").text();
 					if (empresa.length() - 1 == empresa.lastIndexOf("?")) {
 						// Retirar o termo "Por que?"
 						System.out.println(ANSI_YELLOW+"Empresa: " +ANSI_RESET+empresa.substring(0, empresa.indexOf("P")));
-					} else {
+					}
+					else if(empresa.indexOf(",")>0) {
+						System.out.println(ANSI_YELLOW+"Empresa: " +ANSI_RESET+empresa.substring(0, empresa.indexOf(",")));
+					}
+					else {
+					
 						System.out.println(ANSI_YELLOW+"Empresa: " +ANSI_RESET+ empresa);
 					}
+					//Salario
 					String salario = info.getElementsByClass("sc-esjQYD kYLuC").text();
 					System.out.println(ANSI_YELLOW+"Salario: "+ANSI_RESET + salario);
+					//Cidade
+					if(cidade.indexOf("(")>0) {
+					System.out.println(ANSI_YELLOW+"Cidade(s): "+ANSI_RESET + cidade.substring(0,cidade.indexOf("(")));
+					}
+					else {
+						System.out.println(ANSI_YELLOW+"Cidade(s): "+ANSI_RESET + cidade);
+					}
+	                //Vagas
 					String vagas = info.getElementsByTag("strong").text();
 					System.out.println(ANSI_YELLOW+"Quantidade de Vagas: "+ANSI_RESET + vagas.substring(0, vagas.indexOf(":")));
+					
+					
 
 				}
 				j /= 20;
@@ -50,8 +70,8 @@ public class Program {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-
+			}		
+			
 		}
 
 	}
