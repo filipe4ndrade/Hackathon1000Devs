5dece6523f39384240a5b676f0c442bf6dfcd771
José Filipe <58564278+filipe4ndrade@users.noreply.github.com>
2022-11-06 19:42:35 -0300

    importacao do InputMismatchException;

--- src/entities/InspecionarPagina.java
+++ src/entities/InspecionarPagina.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.InputMismatchException;
 public class InspecionarPagina {
 
 	private static final int totalPaginas = 9;
